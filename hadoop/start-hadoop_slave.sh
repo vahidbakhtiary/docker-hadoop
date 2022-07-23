76824ed60acda5c88d1eac3a2d7838a89f28d684
vahidbakhtiary <vahidbakhtiary@hotmail.com>
2022-07-23 11:03:41 +0430

    Update start-hadoop_slave.sh

--- hadoop/start-hadoop_slave.sh
+++ hadoop/start-hadoop_slave.sh
@@ -2,7 +2,7 @@
 
 
 # the default node number is 2
-N=${1:-5}
+N=${1:-3}
 
 # start hadoop slave container
 i=1
@@ -15,7 +15,7 @@ do
 	                --name hadoop-slave1-$i \
 	                --hostname hadoop-slave1-$i \
 					-v dataNode$i:/usr/local/hadoop/hdfs/datanode \
-	                 hadoop-eco:1.0 &> /dev/null
+	                 vahidbakhtiary/hadoop-eco:1.0 &> /dev/null
 	i=$(( $i + 1 ))
 done 
 
