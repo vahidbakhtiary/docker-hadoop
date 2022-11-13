25ae50e0c72477b1a1d7b9e1347c089c416916e5
vahid <vahidbakhtiary@hotmail.com>
2022-11-13 06:02:11 -0500

    add spark

--- spark/build.sh
+++ spark/build.sh
@@ -18,24 +18,5 @@ HADOOP_VERSION="3"
   -t spark-worker .
 
 
-# -- Building the Images
-
-# docker build \
-#   -f cluster-base/Dockerfile \
-#   -t cluster-base .
-
-# docker build \
-#   --build-arg spark_version="${SPARK_VERSION}" \
-#   --build-arg hadoop_version="${HADOOP_VERSION}" \
-#   -f spark-base/Dockerfile \
-#   -t spark-base .
-
-# docker build \
-#   -f spark-master/Dockerfile \
-#   -t spark-master .
-
-docker build \
-  -f spark-worker/Dockerfile \
-  -t spark-worker .
 
  
